473d6e0f732b0a0f634bd41d7ffa35459ccfd5d9
JW <parkjinwoo037@gmail.com>
2025-10-01 18:41:49 +0000

    finished

--- Assignment_1/cs_pa1/tf32.c
+++ Assignment_1/cs_pa1/tf32.c
@@ -268,10 +268,7 @@ double tf322double(tf32 in) {
     // extract exp
     int exp = 0;
 
-    for (int i = 7; i >= 0; i--)
-    {
-        exp |= (((in >> (i + 10)) & 1) << i);
-    }
+    exp = (in >> 10) & 0xFF;
 
     // extract mantissa
     int mantissa = 0;
@@ -362,11 +359,8 @@ tf32 tf32_add(tf32 a, tf32 b) {
     int exp1 = 0;
     int exp2 = 0;
 
-    for (int i = 7; i >= 0; i--)
-    {
-        exp1 |= (((a >> (i + 10)) & 1) << i);
-        exp2 |= (((b >> (i + 10)) & 1) << i);
-    }
+    exp1 = (a >> 10) & 0xFF;
+    exp2 = (b >> 10) & 0xFF;
 
     // extract mantissas
     int mantissa1 = 0;
@@ -378,27 +372,27 @@ tf32 tf32_add(tf32 a, tf32 b) {
         mantissa2 |= ((b >> i) & 1) << i;
     }
 
+    // check if subtracting same number
+    if (sign1 != sign2 && exp1 == exp2 && mantissa1 == mantissa2)
+    {
+        return result;
+    }
+
     // NaN or infinity check
 
     // if either value is NaN, return an arbitrary NaN
     if (exp1 == 255 && mantissa1 != 0)
     {
-        unsigned long long dres = ((unsigned long long) sign1 << 63) | (0x7FFULL << 52) | 1; 
-        
-        union { unsigned long long u; double f; } u;
-        u.u = dres;
+        result = (sign1 << 18) | (0xFF << 10) | 1;
 
-        return u.f;
+        return result;
     }
 
     // when number 2 is NaN
     if (exp2 == 255 && mantissa2 != 0){
-        unsigned long long dres = ((unsigned long long) sign2 << 63) | (0x7FFULL << 52) | 1; 
-        
-        union { unsigned long long u; double f; } u;
-        u.u = dres;
+        result = (sign2 << 18) | (0xFF << 10) | 1;
 
-        return u.f;
+        return result;
     }
 
     // infinity check (both)
@@ -406,42 +400,30 @@ tf32 tf32_add(tf32 a, tf32 b) {
     {
         // if positive infty + negative infty, return positive NaN
         if ((sign1 != sign2)) {
-            unsigned long long dres = ((unsigned long long) sign1 << 63) | (0x7FFULL << 52) | 1; 
-            
-            union { unsigned long long u; double f; } u;
-            u.u = dres;
+            result = (0 << 18) | (0xFF << 10) | 1;
 
-            return u.f;
+            return result;
         }
         else {  // same sign, return infinity with the sign kept
-            unsigned long long dres = ((unsigned long long) sign1 << 63) | (0x7FFULL << 52); 
-            
-            union { unsigned long long u; double f; } u;
-            u.u = dres;
+            result = (sign1 << 18) | (0xFF << 10);
 
-            return u.f;
+            return result;
         }
     }
 
     // only one is infinity
     if (exp1 == 255 && mantissa1 == 0){
         // return infinity with retained sign
-        unsigned long long dres = ((unsigned long long) sign1 << 63) | (0x7FFULL << 52); 
-            
-        union { unsigned long long u; double f; } u;
-        u.u = dres;
+        result = (sign1 << 18) | (0xFF << 10);
 
-        return u.f;
+        return result;
     }
 
     if (exp2 == 255 && mantissa2 == 0){
         // return infinity with retained sign
-        unsigned long long dres = ((unsigned long long) sign2 << 63) | (0x7FFULL << 52); 
-            
-        union { unsigned long long u; double f; } u;
-        u.u = dres;
+        result = (sign2 << 18) | (0xFF << 10);
 
-        return u.f;
+        return result;
     }
 
 
@@ -449,62 +431,478 @@ tf32 tf32_add(tf32 a, tf32 b) {
 
 
     // adding up
-    // compare exps
+    // compare exps, expA is the larger one
+    int signA, signB, expA, expB, manA, manB;
+
     if (exp1 > exp2) {
-        int shift = exp1 - exp2;
+        signA = sign1;
+        signB = sign2;
+
+        expA = exp1;
+        expB = exp2;
+
+        // consider implicit leading 1, consider subnormal values
+        manA = (exp1 ? ((1 << 10) | mantissa1) : mantissa1);
+        manB = (exp2 ? ((1 << 10) | mantissa2) : mantissa2);
+    }
+    else { // less than or equal to
+        signA = sign2;
+        signB = sign1;
+
+        expA = exp2;
+        expB = exp1;
 
         // consider implicit leading 1
-        manA = mantissa1 | (1 << 10);
-        manB = lbits | (1 << 10);
-
-        // removed bits and leftover bits
-        int rbits = manB & ((1 << shift) - 1);
-        int manB = manB >> shift;
-
-        // compute mantissa sum
-
-        int smantissa = manA + manB;
-
-        // if 
-        if (smantissa >= (1 << 11))
-        
-
-    }
-    else if (exp1 < exp2){
-
-    }
-    else{   // same value
-
+        manA = (exp2 ? ((1 << 10) | mantissa2) : mantissa2);
+        manB = (exp1 ? ((1 << 10) | mantissa1) : mantissa1);
     }
 
+    int shift = expA - expB;
 
-    // check if it overflows
+    // removed bits and leftover bits
+    int rbits = manB & ((shift >= 32 ? -1 : (1 << shift) - 1));
+    int lbits = manB >> shift;
+    int smantissa;
 
 
+    // compute mantissa sum or difference
+    if (signA == signB){ // sum
+        smantissa = manA + lbits;
+    }
+    else {  // difference
+        smantissa = manA - lbits;
+    }
+
+    // if mantissa overflows, change exponent
+    if (smantissa >= (1 << 11)){
+        smantissa >>= 1;
+        expA++;
+
+        // if overflows to infinity
+        if (expA >= 255) {  
+            result = (signA << 18) | (0xFF << 10); 
+            
+            return result;
+        }
+    }
+
+    // if mantissa underflows, change exponent
+    while (smantissa > 0 && smantissa < (1 << 10) && expA > 0){
+        smantissa <<= 1;
+        expA--;
+    }
+
+    // if subnormal, just calculate as usual
+
+    int mant_out = smantissa & ((1 << 10) - 1); // keep only 10 LSBs (remove implied 1)
+    int guard  = smantissa & 1;
+    int sticky = (rbits != 0);
+    if (guard && (sticky || (mant_out & 1))) {
+        mant_out++;
+        if (mant_out == (1 << 10)) {
+            mant_out = 0;
+            expA++;
+        }
+    }
+
+    // reconstruct number and return
+    result = (signA << 18) | (expA << 10) | (mant_out);
 
     return result;
 }
 
 tf32 tf32_mul(tf32 a, tf32 b) {
     tf32 result = 0;
-    /*
-    fill this function
-    */
+
+     // Extract sign bits
+    int sign1 = (a >> 18) & 1;
+    int sign2 = (b >> 18) & 1;
+
+    // extract exps
+    int exp1 = 0;
+    int exp2 = 0;
+
+    exp1 = (a >> 10) & 0xFF;
+    exp2 = (b >> 10) & 0xFF;
+
+
+    // extract mantissas
+    int mantissa1 = 0;
+    int mantissa2 = 0;
+
+    for (int i = 9; i >= 0; i--)
+    {
+        mantissa1 |= ((a >> i) & 1) << i; // adjust
+        mantissa2 |= ((b >> i) & 1) << i;
+    }
+
+
+    // NaN and infinity check
+    // if one of them is NaN, return NaN
+    if (exp1 == 255 && mantissa1 != 0)
+    {
+        result = (sign1 << 18) | (0xFF << 10) | 1;
+
+        return result;
+    }
+
+    // when number 2 is NaN
+    if (exp2 == 255 && mantissa2 != 0){
+        result = (sign2 << 18) | (0xFF << 10) | 1;
+
+        return result;
+    }
+
+    // infinity check (both)
+    if (((exp1 == 255 && mantissa1 == 0) || (exp2 == 255 && mantissa2 == 0)))
+    {
+        // if multiplying zero and infinity, return NaN
+        if ((exp1 == 0 && mantissa1 == 0) || (exp2 == 0 && mantissa2 == 0))
+        {
+            // +/- NaN
+            result = ((sign1 ^ sign2) << 18) | (0xFF << 10) | 1;
+
+            return result;
+        }
+
+        // when multiplying with normal value or other infinity, if sign is different, return negative infinity, otherwise return positive infinity
+        result = ((sign1 ^ sign2) << 18) | (0xFF << 10);
+
+        return result;
+    }
+
+
+    
+
+
+    // End of NaN and infinity check
+    // implicit leading 1, handle subnormal value
+    if (exp1 == 0) {
+        exp1 = -126; 
+    } 
+    else {
+        mantissa1 |= (1 << 10);
+        exp1 = exp1 - 127; // remove bias
+    }
+
+    if (exp2 == 0) {
+        exp2 = -126;
+    } 
+    else {
+        mantissa2 |= (1 << 10);
+        exp2 = exp2 - 127; //remove bias
+    }
+
+
+    int s = sign1 ^ sign2;
+    int e = exp1 + exp2 + 127;
+    long long mprod = (long long) mantissa1 * (long long) mantissa2;
+    int m, shift;
+
+    int guard, sticky;
+
+    // normalization step
+    if (mprod & (1LL << 21)) {
+        m = mprod >> 11;
+        guard  = (mprod >> 10) & 1;
+        sticky = (mprod & ((1 << 10) - 1)) != 0;
+        e++;
+    }
+    else {
+        m = mprod >> 10;
+        guard  = (mprod >> 9) & 1;
+        sticky = (mprod & ((1 << 9) - 1)) != 0;
+    }
+
+
+    // if overflows to infinity
+    if (e >= 255) {  
+        result = (s << 18) | (0xFF << 10); 
+        
+        return result;
+    }
+
+    
+
+    // mantissa will never underflow
+
+    // in case value becomes subnormal
+    if (e <= 0) {
+        int shift = 1 - e;
+        if (shift < 32) {
+            // shift mantissa into subnormal range
+            m >>= shift;
+        } else {
+            m = 0;
+        }
+        e = 0;
+    }
+
+    //printf("%d\n", e);
+
+    if (guard && (sticky || (m & 1))) {
+        m++;
+        if (m == (1 << 11)) {
+            m >>= 1;
+            e++;
+        }
+    }
+
+    int mant_out = m & 0x3FF; // keep only 10 LSBs (remove implied 1)
+
+    // reconstruct number and return
+    result = (s << 18) | (e << 10) | (mant_out);
+
+    
     return result;
 }
 
 tf32 tf32_div(tf32 a, tf32 b) {
-    tf32     result = 0;
-    /*
-    fill this function
-    */
+    tf32 result = 0;
+    
+    // Extract sign bits
+    int sign1 = (a >> 18) & 1;
+    int sign2 = (b >> 18) & 1;
+
+    // extract exps
+    int exp1 = 0;
+    int exp2 = 0;
+
+    exp1 = (a >> 10) & 0xFF;
+    exp2 = (b >> 10) & 0xFF;
+
+
+    // extract mantissas
+    int mantissa1 = 0;
+    int mantissa2 = 0;
+
+    for (int i = 9; i >= 0; i--)
+    {
+        mantissa1 |= ((a >> i) & 1) << i; // adjust
+        mantissa2 |= ((b >> i) & 1) << i;
+    }
+
+
+    // NaN and infinity check
+    // if one of them is NaN, return NaN
+    if (exp1 == 255 && mantissa1 != 0)
+    {
+        result = (sign1 << 18) | (0xFF << 10) | 1;
+
+        return result;
+    }
+
+    // when number 2 is NaN
+    if (exp2 == 255 && mantissa2 != 0){
+        result = (sign2 << 18) | (0xFF << 10) | 1;
+
+        return result;
+    }
+
+    // infinity check (both)
+    // if dividing infinity by infinity, return NaN
+    if (((exp1 == 255 && mantissa1 == 0) && (exp2 == 255 && mantissa2 == 0)))
+    {
+        // +/- NaN
+        result = ((sign1 ^ sign2) << 18) | (0xFF << 10) | 1;
+
+        return result;
+    }
+
+    // if dividing infinity by different numbers
+    if (exp1 == 255 && mantissa1 == 0){
+        // return infinity if dividing infinity by any zero
+        if (exp2 == 0 && mantissa2 == 0){
+            result = (sign1 << 18) | (0xFF << 10);
+
+            return result;
+        }
+        else {  // any other number that is nonzero, noninfty, nonNaN -> keep sign
+            result = ((sign1 ^ sign2) << 18) | (0xFF << 10);
+
+            return result;
+        }
+    }
+
+    // Zero check
+    if (exp1 == 0 && mantissa1 == 0) {
+        // dividing zero by zero = positive NaN
+        if (exp2 == 0 && mantissa2 == 0) {
+            result = (0 << 18) | (0xFF << 10) | 1;
+
+            return result;
+        }
+        else {  // infinity or normal value
+            // return +/- zero
+            result |= ((sign1 ^ sign2) << 18);
+
+            return result;
+        }
+    }
+
+    // normal values divided by zero or infinity
+    // divided by zero => +/- infinity
+    if (exp2 == 0 && mantissa2 == 0) {
+        result = ((sign1 ^ sign2) << 18) | (0xFF << 10);
+
+        return result;
+    }
+
+    // divided by infinity => +/- zero
+    if (exp2 == 255 && mantissa2 == 0){
+        result = ((sign1 ^ sign2) << 18);
+
+        return result;
+    }
+
+    // end of NaN and infinity check
+
+    // get implicit leading 1 except for small num
+    if (exp1 != 0) mantissa1 |= (1 << 10);
+    if (exp2 != 0) mantissa2 |= (1 << 10);
+
+    // compute exp value
+    int e = exp1 - exp2 + 127;
+    int s = sign1 ^ sign2;
+
+    // Newton Raphson Algorithm
+    // Scale mantissa into fixed point value
+    int x = mantissa2 << (20 - 10);
+
+    // make reciprocal of b in fixed point
+    int fp_shift = 20;
+    
+    // convert mantissa to fixed point
+    long long m2_fixed = ((long long)mantissa2 << fp_shift) / (1 << 10);
+    int y = ( (3 << (fp_shift-1)) - (m2_fixed >> 1) ); // fp starting point (1 didn't work :/)
+
+    // algorithm
+    for (int i = 0; i < 5; i++) {
+        long long prod = ((long long) m2_fixed * y) >> fp_shift;
+        long long term = ((1LL << (fp_shift + 1)) - prod); // (2 - m2*y)
+        y = (int)(((long long) y * term) >> fp_shift);
+
+    }
+
+    // multiply mantissa and y
+    long long m1_fixed = ((long long)mantissa1 << fp_shift) / 1024;
+    long long M = (m1_fixed * (long long)y) >> fp_shift;
+
+
+    // Normalize values
+    if (M >= (2LL << fp_shift)) { M >>= 1; e++; }
+    else if (M < (1LL << fp_shift)) { M <<= 1; e--; }
+    
+    // fractional part to round
+    long long frac = M - (1LL << fp_shift);
+
+
+    // Round mantissa
+    int res   = (int)((frac >> (fp_shift - 10)) & 0x3FF);   // bits to keep
+    int guard = res & 1;    // LSB of result
+    int roundb = (int)((frac >> (fp_shift - 11)) & 1);  // first removed
+    int sticky = ( (frac & ((1LL << (fp_shift - 11)) - 1)) != 0 );  // OR of rest
+
+    // round to even
+    if (roundb && (sticky || guard)) {
+        res++;
+        if (res == (1 << 10)) {          // carry into hidden 1
+            res = 0;
+            e++;
+        }
+    }
+
+    // reconstruct value
+    result = (s << 18) | ((e & 0xFF) << 10) | (res & ((1 << 10) - 1));
+
     return result;
 }
 
 int tf32_compare(tf32 a, tf32 b) {
     int result = 0;
-    /*
-    fill this function
-    */
-    return result;
+    // 1 if a > b, 0 if a == b, -1 if a < b
+    // Extract sign bits
+    int sign1 = (a >> 18) & 1;
+    int sign2 = (b >> 18) & 1;
+
+    // extract exps
+    int exp1 = 0;
+    int exp2 = 0;
+
+    exp1 = (a >> 10) & 0xFF;
+    exp2 = (b >> 10) & 0xFF;
+
+
+    // extract mantissas
+    int mantissa1 = 0;
+    int mantissa2 = 0;
+
+    for (int i = 9; i >= 0; i--)
+    {
+        mantissa1 |= ((a >> i) & 1) << i; // adjust
+        mantissa2 |= ((b >> i) & 1) << i;
+    }
+
+    // NaN, 0, infinity check
+    // if there exists any NaN value
+    if ((exp1 == 255 && mantissa1 != 0) || (exp2 == 255 && mantissa2 != 0)) {
+        return -2;
+    }
+
+    
+    if ((exp1 == 255 && mantissa1 == 0)) {
+        // if a is positive infinity
+        if (sign1 == 0){
+            // if b is positive infinity
+            if (exp2 == 255 && mantissa2 == 0 && sign2 == 0) {
+                return 0;
+            }
+            else {  // if not, guaranteed to be a > b
+                return 1;
+            }
+        }
+
+        // if a is negative infinity
+        if (sign1 == 1){
+            // if b is negative infinity
+            if (exp2 == 255 && mantissa2 == 0 && sign2 == 1) {
+                return 0;
+            }
+            else {  // if not, guaranteed to be a < b
+                return -1;
+            }
+        }
+    }
+
+    // if both are zeroes return 0
+    if ((exp1 == 0 && mantissa1 == 0) && (exp2 == 0 && mantissa2 == 0)) {
+        return 0;
+    }
+
+    // end of NaN, 0, infinity check
+
+    // checking normal values...
+    // normalize
+    if (exp1 != 0) mantissa1 |= (1 << 10);
+    if (exp2 != 0) mantissa2 |= (1 << 10);
+    
+    // check signs first
+    if (sign1 == 0 && sign2 == 1) {
+        return 1;
+    }
+
+    if (sign1 == 1 && sign2 == 0) {
+        return -1;
+    }
+
+    // compare exp next
+    if (exp1 > exp2) return (sign1 == 0) ? 1 : -1;  // if positive, higher exp is greater
+    if (exp1 < exp2) return (sign1 == 0) ? -1 : 1;
+
+    // compare mantissas next
+    if (mantissa1 > mantissa2) return (sign1 == 0) ? 1 : -1;
+    if (mantissa1 < mantissa2) return (sign1 == 0) ? -1: 1;
+
+    // else return 0 (equal)
+    return 0;
 }
